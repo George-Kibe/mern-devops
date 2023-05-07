5328eeb9594647e4ad181df8861da828fd8cbe65
George-Kibe <georgekibewambui@gmail.com>
2023-05-07 22:01:21 +0300

    individual tweet UI

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,6 +1,20 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 import { TweetType } from '../types'
+import { Entypo, EvilIcons } from '@expo/vector-icons';
+
+type IconButtonProps = {
+    icon: React.ComponentProps<typeof EvilIcons>["name"]
+    text?: string | number
+}
+const IconButton = ({icon, text}: IconButtonProps) => {
+    return (
+      <View style={{flexDirection: "row", alignItems: "center"}}>
+        <EvilIcons name={icon} size={22} color={"gray"} />
+        <Text style={{fontSize: 12}}>{text}</Text>
+      </View>
+    )
+}
 
 type TweetProps = {
     tweet: TweetType
@@ -11,8 +25,20 @@ const Tweet = ({tweet}: TweetProps) => {
     <View style={styles.container}>
       <Image source={{uri: tweet.user.image}} style={styles.image}/>
       <View style={styles.tweetContainer}>
-        <Text style={styles.name}>{tweet.user.name}</Text>
+        <View style={{flexDirection: "row"}}>
+          <Text style={styles.name}>{tweet.user.name}</Text>
+          <Text style={styles.username}>@{tweet.user.username} •2mins</Text>
+          <Entypo name="dots-three-horizontal" size={20} color="gray" style={{marginLeft:"auto"}} />
+        </View>
         <Text style={styles.content}>{tweet.content}</Text>
+        { tweet.image && <Image source={{ uri:tweet.image}} style={styles.tweetImage}/> }  
+        <View style={styles.tweetIcons}>
+          <IconButton icon="comment" text={tweet.numberOfComments}/>
+          <IconButton icon="retweet" text={tweet.numberOfRetweets}/>
+          <IconButton icon="heart" text={tweet.numberOfLikes}/>
+          <IconButton icon="chart" text={tweet.impressions}/>
+          <IconButton icon="share-apple" />
+        </View>      
       </View>     
     </View>
   )
@@ -41,9 +67,24 @@ const styles = StyleSheet.create({
     name: {
       fontWeight: "600"
     },
+    username: {
+      color: "gray",
+      marginLeft: 5,
+    },
     content: {
       lineHeight: 20,
       marginTop: 5
+    },
+    tweetImage: {
+        width: "100%",
+        aspectRatio: 16/9,
+        marginVerical: 10,
+        borderRadius: 15
+    },
+    tweetIcons: {
+        flexDirection: "row",
+        marginVertical: 5,
+        justifyContent: "space-between"
     }
   });
   
