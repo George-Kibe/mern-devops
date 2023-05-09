c17760c01116c97907eeb3ae372045f59de6cb6e
George-Kibe <georgekibewambui@gmail.com>
2023-05-09 16:52:22 +0300

    User crud operations and tests

--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -1,34 +1,78 @@
 import { Router } from "express";
+import {PrismaClient} from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
 
 // User CRUD Operations
 // Create a user
-router.post("/", (req,res) => {
-    res.status(501).json({error: "Not Implemented!"});
+router.post("/", async(req,res) => {
+    const {email, name, username} = req.body;
+    // console.log(email, name, username)
+    try {
+        const newUser = await prisma.user.create({
+            data: {
+                email, name, username,
+                bio: "Hello There, I'm new on Twitter"
+            }
+        })
+        if(newUser){
+            res.status(201).json(newUser)
+        }
+    } catch (error) {
+        res.status(422).json({error: "Unprocessable Entity! Email and Username must be unique"});
+        return;
+    }
 });
 
 // List users
-router.get("/", (req,res) => {
+router.get("/", async(req,res) => {
+    const allUsers = await prisma.user.findMany();
+    if(allUsers){
+        res.status(200).json(allUsers)
+    }
     res.status(501).json({error: "Not implemeted"})
 });
 
 // get one user
-router.get("/:id", (req,res) => {
+router.get("/:id", async(req,res) => {
     const {id} = req.params;
+    const user = await prisma.user.findUnique({where: {id: Number(id)}})
+    if(user){
+        res.status(200).json(user)
+    }
     res.status(501).json({error: `Not implemented: ${id}`})
 });
 
 // update one user
-router.put("/:id", (req,res) => {
+router.put("/:id", async(req,res) => {
     const {id} = req.params;
-    res.status(501).json({error: `Not implemented: ${id}`})
+    const {name, bio, image} = req.body;
+    // console.log(email, name, username)
+    try {
+        const newUser = await prisma.user.update({
+            where: {id: Number(id)},
+            data: { name, bio, image }
+        })
+        if(newUser){
+            res.status(201).json(newUser)
+        }
+    } catch (error) {
+        res.status(422).json({error: "Unprocessable Entity! Email and Username must be unique"});
+        return;
+    }
 });
 
 // delete one user
-router.delete("/:id", (req,res) => {
+router.delete("/:id", async(req,res) => {
     const {id} = req.params;
-    res.status(501).json({error: `Not implemented: ${id}`})
+    try {
+        await prisma.user.delete({where: {id: Number(id)}})
+        res.status(200).json({message: `User id ${id} deleted successfully!`})
+    } catch (error) {
+        res.status(404).json({message: `User id ${id} Not Deleted or Not Found!`});
+        return
+    }    
 });
 
 
